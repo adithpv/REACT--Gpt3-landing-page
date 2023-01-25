d4bc64020f5a27339fffa3f82a98a716ec413a60
Adith pv <adithpv2014@gmail.com>
2023-01-25 15:42:33 +0530

    first commit

---
+++ src/containers/footer/Footer.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import "./footer.css";
+
+import gpt3Logo from "../../assets/logo.svg";
+
+const Footer = () => {
+  return (
+    <div className="gpt3__footer section__padding">
+      <div className="gpt3__footer-heading">
+        <h1 className="gradient__text">
+          Do you want to step into the future before others
+        </h1>
+      </div>
+      <div className="gpt3__footer-btn">
+        <p>Request Early Access</p>
+      </div>
+      <div className="gpt3__footer-links">
+        <div className="gpt3__footer-links_logo">
+          <img src={gpt3Logo} alt="Logo" />
+          <p>Alderin ST K12 Dk Alpoer,All rights Reserved</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Company</h4>
+          <p>Terms and Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Get in Touch</h4>
+          <p>Alderin ST K12 Dk Alpoer</p>
+          <p>878-125489</p>
+          <p>info@gpt3.com</p>
+        </div>
+      </div>
+      <div className="gpt3__footer-copyright">
+        <p>2022 GPT-3. All rights reserved.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import People from "../../assets/people.png";
+import ai from "../../assets/ai.png";
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div className="gpt3__header section__padding " id="home">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">
+          Let's Build Something amazing with GPT-3 OpenAI
+        </h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde quo
+          est, quam a ipsam aspernatur corporis quasi aliquid neque. Quod
+          corporis totam optio asperiores quisquam enim, tempore nesciunt.
+        </p>
+        <div className="gpt3__header-content__input">
+          <input type="email" placeholder="Your Email Address" />
+          <button type="button">Get Started</button>
+        </div>
+        <div className="gpt3__header-content__people">
+          <img src={People} alt="people" />
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+      </div>
+      <div className="gpt3__header-image">
+        <img src={ai} alt="ai" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
